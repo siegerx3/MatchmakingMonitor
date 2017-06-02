923d168f84f5ecbf98a3c6c6b0840cb862f2a37b
siegerx3 <benjamin.heilmann@yahoo.de>
2017-06-02 15:02:24 +0200

    Added settings upgrade logic
    Fixed SEA region in default settings

---
+++ Client/Services/UpgradeSettings.cs
@@ -0,0 +1,23 @@
+﻿using MatchMakingMonitor.config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchmakingMonitor.Services
+{
+  public static class UpgradeSettings
+  {
+    public static readonly Version InitialVersion = new Version("1.0.0.0");
+    public static void Upgrade(SettingsJson settings, Version settingsVersion)
+    {
+      if (settingsVersion.CompareTo(new Version("1.0.0.0")) <= 0)
+        From1000(settings);
+    }
+    public static void From1000(SettingsJson settings)
+    {
+      settings.AppIds[3].Region = Region.ASIA;
+    }
+  }
+}
